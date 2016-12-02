d96235db2b94f506b4a4e67f2cdb645bbb044371
Chris Raiskin <chris.raiskin@standard.com>
2016-12-02 13:19:25 -0800

    first step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
+import { Ng2TourOfHerosPage } from '../../e2e/app.po';
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+
+    <h1>{{title}}</h1>
+    <div class="header-bar"></div>
+    <h2>{{hero.name}} details!</h2>
+    <div>
+      <label>id: </label>{{hero.id}}
+    </div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  title:string = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'windstorm'
+  };
 }
