1ffba92a3d854e2524c989a7dd8c4e4f07e4af4a
CJR 8020 <cjr8020@gmail.com>
2016-12-09 14:03:16 -0800

    delete a hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -48,6 +48,14 @@ export class HeroService {
 
   }
 
+  delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then( () => null )
+      .catch(this.handleError);
+  }
+
   private handleError(error: any): Promise<any> {
     console.error('An error occured', error);  // for demo purposes only
     return Promise.reject(error.message || error);
